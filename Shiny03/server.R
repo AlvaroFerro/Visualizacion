33ae5d04ef9a9b36ed45851fe28fbb8462598466
AlvaroFerro <alvaro.ferro@cunef.edu>
2018-11-19 16:58:46 +0100

    Proyecto 3 de Shiny

--- Shiny03/server.R
+++ Shiny03/server.R
@@ -3,23 +3,59 @@ library(ggplot2)
 
 shinyServer(function(input, output){
   
-  output$grafica <- renderPlot({
+  variables <- reactiveValues(tamanio = 0, 
+                              distribucion = 0, 
+                              orden = 0,
+                              x = 0,
+                              y = 0,
+                              plotInicial = 0,
+                              x3 = 0,
+                              y3 = 0,
+                              z = 0)
+  
+  react <- eventReactive(input$generar, { 
+    variables$tamanio <- isolate(input$tamanio)
+    variables$distribucion <- isolate(input$correlacion)
+    variables$orden <- isolate(input$orden)
     
-    input$generar
-    tamanio <- isolate(input$tamanio)
-    distribucion <- isolate(input$correlacion)
+    meanA <- -0.5
+    meanB <- 0.5
+    sdA <- 1
+    sdB <- 2
     
-    orden <- isolate(input$orden)
+    # Generate from independent standard normals
+    variables$x <- rnorm(variables$tamanio, 0, 1)
+    variables$y <- rnorm(variables$tamanio, 0, 1)
     
-    plot(as.numeric(tamanio), cor(as.numeric(tamanio)))
-    #ggplot(tamanio, aes(x=tamanio, y = cor(tamanio))) + geom_point() + geom_smooth(method=lm, se=FALSE)
+    # Transform
+    x2 <- variables$x # could be avoided
+    y2 <- variables$distribucion*variables$x + sqrt(1 - variables$distribucion^2)*variables$y
+    
+    # Fix up means and standard deviations
+    variables$x3 <- meanA + sdA*x2
+    variables$y3 <- meanB + sdB*y2
+
+    
+    variables$z <- as.data.frame(cbind(variables$x3, variables$y3))
+    
+    variables$plotInicial <- ggplot(variables$z) + geom_point(aes(x = variables$x3, y = variables$y3)) + xlab('X') + ylab('Y')
+    variables$plotInicial 
   })
   
-
+  observeEvent(input$modelo, {
+    variables$orden <- isolate(input$orden)
+    
+    #variables$plotInicial + 
+    #append_layers(variables$plotInicial, stat_smooth(aes(x = variables$x3, y = variables$y3), method = "lm", formula = y ~ poly(x, input$orden), se = F, size = 0.25))
+  
+    output$grafica <- renderPlot(variables$plotInicial + stat_smooth(aes(x = variables$x3, y = variables$y3), method = "lm", formula = y ~ poly(x, variables$orden), se = F, size = 0.25))
+    })
+  
+  
+  output$grafica <- renderPlot({
+    react()
+    #ggplot(variables$z) + geom_point(aes(x = variables$x3, y = variables$y3))
+  })
+  
+  
 })
-
-
-#Orden polinomial 1/2/3, orden 1 es una linea
-#Cuando se pulsa añadir se añade una linea
-#Genero aparecen, pulso orden y aparecen
-#Orden 2 en ggplot
--- Shiny03/ui.R
+++ Shiny03/ui.R
@@ -2,29 +2,20 @@ library(shiny)
 
 shinyUI(
   fluidPage(
-    titlePanel(h2("Mi primer dashboard")),
+    titlePanel(h2("Representación polinómica en scatter-plot")),
     sidebarLayout(
       sidebarPanel(
-        textInput("tamanio", label = "Tamaño de la muestra", value = "10"),
-        textInput('correlacion', label = 'Correlacion', value = 'Nivel de correlacion...'),
-        actionButton("generar", label = "Generar", icon('calendar')),
+        numericInput("tamanio", label = "Tamaño de la muestra", value = 500, min = 100, max = 30000, step = 100),
+        numericInput('correlacion', label = 'Correlacion', value = 0, min = -1, max = 1, step = 0.1),
+        actionButton("generar", label = "Generar", icon('500px')),
         
-        textInput('orden', label = 'Orden del polinomio', value = 'Orden polinomial...'),
-        actionButton('modelo', label = 'Añadir modelo')
+        numericInput('orden', label = 'Orden del polinomio', value = 1, min = 0),
+        actionButton('modelo', label = 'Añadir modelo', icon('500px'))
       ),
       
       mainPanel(
         plotOutput('grafica')
       )
-    
     )
   )
 )
-
-### Interfaz así, sidebar con tamaño (numeric input), correlacion (numeric input), boton (generar)
-#Diagrama de dispersion normal de 2 variables con correlacion - lo buscamos
-#Modelo lm
-
-#Hay otra seccion abajo
-#Orden (numeric - polinomial)
-#Boton añadir modelo
